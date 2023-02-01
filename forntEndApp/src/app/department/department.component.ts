e048b1509557c9f63319d3bf2bf8e30a7f331e09
BryanDLC <delacruzbryanvt@gmail.com>
2023-02-01 14:59:33 -0500

    Validation input Phone Department

--- forntEndApp/src/app/department/department.component.ts
+++ forntEndApp/src/app/department/department.component.ts
@@ -103,4 +103,13 @@ export class DepartmentComponent implements OnInit {
       }
     )
   }
+
+  onlyNumbers(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false
+    }
+    return true
+  }
 }
